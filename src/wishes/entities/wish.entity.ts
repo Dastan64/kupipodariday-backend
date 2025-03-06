d4278246b4c7704b8244f5e3e56e8aa85e661625
Dastan Zhamekeshev <dastan132000@mail.ru>
2025-03-06 16:38:12 +0500

    feat: described main entites

--- src/wishes/entities/wish.entity.ts
+++ src/wishes/entities/wish.entity.ts
@@ -1,1 +1,39 @@
-export class Wish {}
+import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
+import { IsUrl, Length } from 'class-validator';
+
+import { User } from '../../users/entities/user.entity';
+import { Offer } from '../../offers/entities/offer.entity';
+import { BaseEntity } from '../../shared/base.entity';
+
+@Entity()
+export class Wish extends BaseEntity {
+  @Column()
+  @Length(1, 250)
+  name: string;
+
+  @Column()
+  @IsUrl()
+  link: string;
+
+  @Column()
+  @IsUrl()
+  image: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  price: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  raised: number;
+
+  @ManyToOne(() => User, (user) => user.wishes)
+  owner: User;
+
+  @Column({ length: 1024 })
+  description: string;
+
+  @OneToMany(() => Offer, (offer) => offer.item)
+  offers: Offer[];
+
+  @Column({ type: 'decimal' })
+  copied: number;
+}
--- src/wishlists/entities/wishlist.entity.ts
+++ src/wishlists/entities/wishlist.entity.ts
@@ -1,1 +1,27 @@
-export class Wishlist {}
+import { Column, Entity, JoinTable, ManyToMany, ManyToOne } from 'typeorm';
+import { IsUrl, Length } from 'class-validator';
+
+import { User } from '../../users/entities/user.entity';
+import { Wish } from '../../wishes/entities/wish.entity';
+import { BaseEntity } from '../../shared/base.entity';
+
+@Entity()
+export class Wishlist extends BaseEntity {
+  @ManyToOne(() => User, (user) => user.wishlists)
+  owner: User;
+
+  @Column()
+  @Length(1, 250)
+  name: string;
+
+  @Column({ length: 1500 })
+  description: string;
+
+  @Column()
+  @IsUrl()
+  image: string;
+
+  @ManyToMany(() => Wish)
+  @JoinTable()
+  items: Wish[];
+}
